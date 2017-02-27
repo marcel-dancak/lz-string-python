94f425b585a311c812bb8878ab02ecbafc3c333c
Marcel Dancak <dancakm@gmail.com>
2017-02-27 23:23:04 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='lzstring',
+    version='0.1',
+    description='lz-string for python',
+    author='Marcel Dancak',
+    author_email='dancakm@gmail.com',
+    py_modules=['lzstring'],
+    install_requires=[],
+    url='https://github.com/marcel-dancak/lz-string-python',
+    download_url='https://github.com/marcel-dancak/lz-string-python',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ]
+)
